dcc73bbbd499ca217615e7f51abed953bc910c71
alex-softuni <notorious.atanasow@gmail.com>
2026-04-25 10:59:24 +0300

    feat:added proper link nav using thymeleaf feat:added endpoints for login,register,index pages

--- src/main/java/com/example/veritas/web/controller/IndexController.java
+++ src/main/java/com/example/veritas/web/controller/IndexController.java
@@ -3,6 +3,7 @@ package com.example.veritas.web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/")
@@ -14,5 +15,19 @@ public class IndexController {
     public String getIndexPage() {
 
         return "index";
+    }
+
+    @GetMapping("/register")
+    public ModelAndView getRegisterPage() {
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("register");
+        return mav;
+    }
+
+    @GetMapping("/login")
+    public ModelAndView getLoginPage() {
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("login");
+        return mav;
     }
 }
