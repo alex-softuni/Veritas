089081b5e915d148df4f97496aa9e457b75f1de2
alex-softuni <notorious.atanasow@gmail.com>
2026-04-26 12:28:39 +0300

    feat:data validation during login

--- src/main/java/com/example/veritas/web/controller/IndexController.java
+++ src/main/java/com/example/veritas/web/controller/IndexController.java
@@ -1,6 +1,7 @@
 package com.example.veritas.web.controller;
 
 import com.example.veritas.user.service.UserService;
+import com.example.veritas.web.dto.LoginRequest;
 import com.example.veritas.web.dto.RegisterRequest;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -47,9 +49,15 @@ public class IndexController {
     }
 
     @GetMapping("/login")
-    public ModelAndView getLoginPage() {
+    public ModelAndView getLoginPage(@RequestParam(value = "error", required = false) String errorParam) {
         ModelAndView mav = new ModelAndView();
         mav.setViewName("login");
+        mav.addObject("loginRequest", new LoginRequest());
+
+        if (errorParam != null) {
+            mav.addObject("errorMessage", "Incorrect username or password!");
+        }
+
         return mav;
     }
 }
