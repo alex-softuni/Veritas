aeb52db7eb65700dd1615c166134222e6e0347cc
alex-softuni <notorious.atanasow@gmail.com>
2026-04-26 12:15:33 +0300

    feat:data validation during register

--- src/main/java/com/example/veritas/web/controller/IndexController.java
+++ src/main/java/com/example/veritas/web/controller/IndexController.java
@@ -2,8 +2,10 @@ package com.example.veritas.web.controller;
 
 import com.example.veritas.user.service.UserService;
 import com.example.veritas.web.dto.RegisterRequest;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,7 +36,12 @@ public class IndexController {
     }
 
     @PostMapping("/register")
-    public String registerUser(RegisterRequest registerRequest) {
+    public String registerUser(@Valid RegisterRequest registerRequest, BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            return "/register";
+        }
+
         userService.register(registerRequest);
         return "redirect:/login";
     }
